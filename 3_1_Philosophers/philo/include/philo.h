594929c3c5b147cc886f25624f7f59aa9cbb712c
Daegeun Song <eorms6199@gmail.com>
2023-06-23 14:06:28 +0900

    philo temp

---
+++ 3_1_Philosophers/philo/include/philo.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dasong <dasong@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/23 14:03:15 by dasong            #+#    #+#             */
+/*   Updated: 2023/06/23 14:03:19 by dasong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// ./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]
+
+#ifndef PHILO_H
+# define PHILO_H
+
+
+#endif
